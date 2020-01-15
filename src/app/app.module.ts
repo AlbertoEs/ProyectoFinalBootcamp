7f06c84149fabe77815f3f464f2247195c2cb178
Mac <alberto@MacBook-Pro-de-Alberto.local>
2020-01-15 21:51:42 +0100

    Añadido pipe de traducción sin terminar, iniciado el routing y creada la carpeta de interfaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/home/home.component';
+import { TraductorPipe } from './Pipes/traductor.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TraductorPipe
   ],
   imports: [
     BrowserModule,
